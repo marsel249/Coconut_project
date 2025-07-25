53d34867542f2826fdbd70bde3112e97f4bb2e81
Marsel <marsel249@yahoo.com>
2025-07-25 16:33:37 +0300

    .gitignore

--- module_4/Cinescope/api/api_manager.py
+++ module_4/Cinescope/api/api_manager.py
@@ -13,3 +13,17 @@ class ApiManager:
         self.session = session
         self.auth_api = AuthAPI(session)
         self.user_api = UserAPI(session)
+
+'''
+    Pytest->>TestFile: Запуск теста test_register_user()
+    TestFile->>Conftest: Запрос фикстур (api_manager, test_user)
+    Conftest->>Conftest: Создает session → requester → api_manager
+    Conftest->>Conftest: Генерирует test_user
+    TestFile->>AuthAPI: Вызов api_manager.auth_api.register_user(test_user)
+    AuthAPI->>CustomRequester: Делегирует вызов send_request()
+    CustomRequester->>RealAPI: Отправляет POST /register
+    RealAPI-->>CustomRequester: Ответ {id, email, roles}
+    CustomRequester-->>AuthAPI: Объект Response
+    AuthAPI-->>TestFile: Возвращает response
+    TestFile->>TestFile: Проверки (assert)
+    TestFile-->>Pytest: Результат (Pass/Fail)'''
